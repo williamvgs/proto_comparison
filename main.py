a0e56c7ef2a6738ccc6447eb122b59b2370f22ad
williamvgs <wilsun30@o365.mrvgs.no>
2024-02-01 08:36:25 +0100

    V1.0 for test purposes

--- main.py
+++ main.py
@@ -49,46 +49,39 @@ def compare_with_line_numbers(file_content1, file_content2):
 
     return differences
 
-
 def main():
     parser = argparse.ArgumentParser(description='Process two files.')
     parser.add_argument('-x', nargs='*', help='Runs a case-sensitive comparison')
     parser.add_argument('-c', nargs='*', help='Counts how many differences there are')
     parser.add_argument('-i', nargs='*', help='Runs a case-insensitive comparison')
     parser.add_argument('-xn', nargs='*', help='Runs a case-sensitive comparison and shows what line the difference is on ')
-    parser.add_argument('-e', '--exit', action='store_true', help='Exit the program after the current iteration')
+    args = parser.parse_args()
 
-    while True:
-        args = parser.parse_args()
-
-
-        if args.x:
-            file_path1, file_path2 = args.x
-            file_content1, file_content2 = fetch_files(file_path1, file_path2)
-            differences = compare(file_content1, file_content2)
-            for diff in differences:
-                print(f">>> {diff[0]}\n>>> {diff[1]}\n")
-        elif args.c:
-            file_path1, file_path2 = args.c
-            differences = compare(file_path1, file_path2)
-            count = len(differences)
-            print(f"\nNumber of differences: {count}")
-        elif args.i:
-            file_path1, file_path2 = args.i
-            file_content1, file_content2 = fetch_files(file_path1, file_path2)
-            differences = compare_case_insensitive(file_content1, file_content2)
-            for diff in differences:
-                print(f">>> {diff[0]}\n>>> {diff[1]}\n")
-        elif args.xn:
-            file_path1, file_path2 = args.xn
-            file_content1, file_content2 = fetch_files(file_path1, file_path2)
-            differences = compare_with_line_numbers(file_content1, file_content2)
-            for diff in differences:
-                print(f"Line {diff[0]}: >>> {diff[1]}\nLine {diff[0]}: >>> {diff[2]}\n")
-        elif args.e:
-            break
-        else:
-            print("Please provide appropriate options. Use -h for help.")
+    if args.x:
+        file_path1, file_path2 = args.x
+        file_content1, file_content2 = fetch_files(file_path1, file_path2)
+        differences = compare(file_content1, file_content2)
+        for diff in differences:
+            print(f">>> {diff[0]}\n>>> {diff[1]}\n")
+    elif args.c:
+        file_path1, file_path2 = args.c
+        differences = compare(file_path1, file_path2)
+        count = len(differences)
+        print(f"\nNumber of differences: {count}")
+    elif args.i:
+        file_path1, file_path2 = args.i
+        file_content1, file_content2 = fetch_files(file_path1, file_path2)
+        differences = compare_case_insensitive(file_content1, file_content2)
+        for diff in differences:
+            print(f">>> {diff[0]}\n>>> {diff[1]}\n")
+    elif args.xn:
+        file_path1, file_path2 = args.xn
+        file_content1, file_content2 = fetch_files(file_path1, file_path2)
+        differences = compare_with_line_numbers(file_content1, file_content2)
+        for diff in differences:
+            print(f"Line {diff[0]}: >>> {diff[1]}\nLine {diff[0]}: >>> {diff[2]}\n")
+    else:
+        print("Please provide appropriate options. Use -h for help.")
 
 if __name__ == "__main__":
     main()
